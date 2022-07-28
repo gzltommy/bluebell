fe80e9b68b6d3eeb7e887ac14baea002029b074c
gouzuoluo <1046292959@qq.com>
2022-07-28 16:09:40 +0800

    将需要认证和不需要认证的接口分开为两个 group 《=》 路径不变

--- routes/routers.go
+++ routes/routers.go
@@ -15,7 +15,7 @@ func SetupRouter(mode string) *gin.Engine {
 		// 用户
 		v1.POST("user/signup", controller.SignupHandler)
 		v1.POST("user/login", controller.LoginHandler)
-		v1.GET("user/refresh-token", controller.RefreshTokenHandler)
+		auth.GET("user/refresh-token", controller.RefreshTokenHandler)
 	}
 
 	return r
