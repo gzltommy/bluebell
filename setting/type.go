800314969f0b170e981b1e0a2c2f3e5642bcabae
gouzuoluo <1046292959@qq.com>
2022-08-03 14:39:09 +0800

    后端完整版 1

--- setting/type.go
+++ setting/type.go
@@ -15,33 +15,35 @@ type Config struct {
 	Redis *RedisConfig `mapstructure:"redis"`
 }
 
-type AuthConfig struct {
-	JwtSecret string `mapstructure:"jwt_secret"`
-	JwtExpire int64  `mapstructure:"jwt_expire"`
-}
+type (
+	AuthConfig struct {
+		JwtSecret string `mapstructure:"jwt_secret"`
+		JwtExpire int64  `mapstructure:"jwt_expire"`
+	}
 
-type LogConfig struct {
-	Level      string `mapstructure:"level"`
-	Filename   string `mapstructure:"filename"`
-	MaxSize    int    `mapstructure:"max_size"`
-	MaxAge     int    `mapstructure:"max_age"`
-	MaxBackups int    `mapstructure:"max_backups"`
-}
+	LogConfig struct {
+		Level      string `mapstructure:"level"`
+		Filename   string `mapstructure:"filename"`
+		MaxSize    int    `mapstructure:"max_size"`
+		MaxAge     int    `mapstructure:"max_age"`
+		MaxBackups int    `mapstructure:"max_backups"`
+	}
 
-type MySQLConfig struct {
-	Host         string `mapstructure:"host"`
-	User         string `mapstructure:"user"`
-	Password     string `mapstructure:"password"`
-	Port         int    `mapstructure:"port"`
-	Dbname       string `mapstructure:"dbname"`
-	MaxOpenConns int    `mapstructure:"max_open_conns"`
-	MaxIdleConns int    `mapstructure:"max_idle_conns"`
-}
+	MySQLConfig struct {
+		Host         string `mapstructure:"host"`
+		User         string `mapstructure:"user"`
+		Password     string `mapstructure:"password"`
+		Port         int    `mapstructure:"port"`
+		Dbname       string `mapstructure:"dbname"`
+		MaxOpenConns int    `mapstructure:"max_open_conns"`
+		MaxIdleConns int    `mapstructure:"max_idle_conns"`
+	}
 
-type RedisConfig struct {
-	Host     string `mapstructure:"host"`
-	Password string `mapstructure:"password"`
-	Port     int    `mapstructure:"port"`
-	DB       int    `mapstructure:"db"`
-	PoolSize int    `mapstructure:"pool_size"`
-}
+	RedisConfig struct {
+		Host     string `mapstructure:"host"`
+		Password string `mapstructure:"password"`
+		Port     int    `mapstructure:"port"`
+		DB       int    `mapstructure:"db"`
+		PoolSize int    `mapstructure:"pool_size"`
+	}
+)
